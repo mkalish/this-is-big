416a1325096ef3052c8b5e941b5e8b1f906e10f8
Patrick Burke <pburke@redhat.com>
2013-03-22 19:08:06 -0400

    Added some styling, new Gear info tab image, and Twitter Slider

--- slidePanels.js
+++ slidePanels.js
@@ -2,16 +2,36 @@ $(function(){
 
     $("#gearTab").click(function(){
         var state = $(this).data('state');
+        var width = $("#gearInfo").width();
         if(state == 'in')
         {
             $("#gearInfo, #gearTab").animate({
-                left:'+=250px'
+                left:'+=' + width + 'px'
             }, 'slow');
             $(this).data('state', 'out');
         }
         else{
             $("#gearInfo, #gearTab").animate({
-                left:'-=250px'
+                 left:'-=' + width + 'px'
+            }, 'slow');
+            $(this).data('state', 'in');
+        }
+    });
+    
+    
+    $("#twitterTab").click(function(){
+        var state = $(this).data('state');
+        var width = $("#twitterInfo").width();
+        if(state == 'in')
+        {
+            $("#twitterInfo, #twitterTab").animate({
+                right:'+=' + width + 'px'
+            }, 'slow');
+            $(this).data('state', 'out');
+        }
+        else{
+            $("#twitterInfo, #twitterTab").animate({
+                 right:'-=' + width + 'px'
             }, 'slow');
             $(this).data('state', 'in');
         }
