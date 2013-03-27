107c1bbdfc3fe321f138099577d862b7dfe86e4a
Michael Kalish <mkalish@mkalish.home>
2013-03-27 17:27:00 -0400

    New audio and new node generation

--- scripts/script.js
+++ scripts/script.js
@@ -3,10 +3,14 @@ var xLocation = 800;
 var yLocation = 500;
 var currentColor = 'red';
 var gearId = 0;
+var nodeId = 0;
+var nodeLocations = [{x:900,y:500},{x:500,y:500},{x:1200, y:600}];
 var canvas;
-var circleRadius = 350;
+var circleRadius = 200;
 var all_gears= [];
+var all_nodes = [];
 var currentAudioId=1;
+var currentAudio;
 var animations = [Raphael.animation({transform: 't0,100'}, 1000),
 		 Raphael.animation({transform: 't0,-100'}, 1000),
 		 Raphael.animation({transform: 't100,0'}, 1000),
@@ -33,14 +37,14 @@ function createNewGear(UISet) {
 	
 	self.increaseSize = increaseSize;
 	function increaseSize() {
-		self.ui.animate({r:25}, self.speed, self.decreaseSize);
+		self.ui.animate({r:10}, self.speed, self.decreaseSize);
 		//makeMove();
 	}
 	
 	
 	self.decreaseSize = decreaseSize;
 	function decreaseSize() {
-		self.ui.animate({r:15}, self.speed, self.increaseSize);
+		self.ui.animate({r:5}, self.speed, self.increaseSize);
 		//makeMove();
 	}
 	
@@ -62,10 +66,41 @@ function createNewGear(UISet) {
 		if (self.currentDirection == 'No direction') {
 			self.currentDirection = Math.floor(Math.random() * directions.length);
 		}
+		switch (self.currentDirection) {
+			case 0:
+				self.yPos += .5;
+				break;
+			case 1:
+				self.yPos += -.5;
+				break;
+			case 2:
+				self.xPos += -.5;
+				break;
+			case 3:
+				self.xPos += .5;
+				break;
+			case 4:
+				self.xPos += -.5;
+				self.yPos += .5;
+				break;
+			case 5:
+				self.xPos += .5;
+				self.yPos += .5;
+				break;
+			case 6:
+				self.xPos += -.5;
+				self.yPos += -.5;
+				break;
+			case 7:
+				self.xPos += .5;
+				self.yPos += -.5;
+				break;
+		}
 		var x2 = self.xPos*self.xPos;
 		var y2 = self.yPos*self.yPos;
-		var radius2 = circleRadius*circleRadius;
-		if (x2+y2 > radius2) {
+		var radius2 = (circleRadius*circleRadius);
+		if ((x2+y2+100) > radius2) {
+			console.log('switching direction'+ (x2+y2) + ',' + radius2);
 			switch (self.currentDirection) {
 			case 0:
 				var possible = [1,6,7];
@@ -100,43 +135,14 @@ function createNewGear(UISet) {
 				self.currentDirection = possible[Math.floor(Math.random()*possible.length)];
 				break;
 			}
-		}
-		switch (self.currentDirection) {
-			case 0:
-				self.yPos += 5;
-				break;
-			case 1:
-				self.yPos += -5;
-				break;
-			case 2:
-				self.xPos += -5;
-				break;
-			case 3:
-				self.xPos += 5;
-				break;
-			case 4:
-				self.xPos += -5;
-				self.yPos += 5;
-				break;
-			case 5:
-				self.xPos += 5;
-				self.yPos += 5;
-				break;
-			case 6:
-				self.xPos += -5;
-				self.yPos += -5;
-				break;
-			case 7:
-				self.xPos += 5;
-				self.yPos += -5;
-				break;
-		}
-		self.ui.animate({transform: 'T'+self.xPos+','+self.yPos},100);
+		} else {
+			self.ui.animate({transform: 'T'+self.xPos+','+self.yPos},10);	
+		}		
 	}
 	
 	self.startMoving = startMoving;
 	function startMoving() {
-		setInterval(function(){makeMove()}, 100);
+		setInterval(function(){makeMove()}, 10);
 	}
 	
 	this.reset = reset;
@@ -145,17 +151,11 @@ function createNewGear(UISet) {
 	}
 }
 
-function createGearUI() {
-	var gearCircle = canvas.circle(1000, 500, 15);
+function createGearUI(xCenter, yCenter) {
+	var gearCircle = canvas.circle(xCenter, yCenter, 5);
 	gearCircle.attr({fill: currentColor});
-	
-	var gearText = canvas.text(xLocation, yLocation, gearId);
-	gearText.attr({'font-size': 12});
-	
-	var gearUI = canvas.set();
-	gearUI.push(gearCircle);
-	gearUI.push(gearText);
-	return gearUI;
+
+	return gearCircle;
 }
 
 function addGear() {
@@ -164,11 +164,50 @@ function addGear() {
 	var newGear = new createNewGear(createGearUI());
 	newGear.startPulse();
 	newGear.startMoving();
-	var currentAudio = new Audio("audio/RHSummit_"+currentAudioId+".wav");
+	var oldAudio = currentAudio;
+	currentAudio = new Audio("audio/RHSummit_"+currentAudioId+".wav");
+	if (oldAudio != null) {
+		oldAudio.mute = true;
+	}
 	currentAudio.loop=true;
 	currentAudio.play();
 	currentAudioId += 1;
 	all_gears.push(newGear);
+}
+
+function createNode(UI,x ,y) {
+	var self = this;
+	self.ui = UI;
+	self.nodes = [];
+	self.xCenter = x;
+	self.yCenter = y;
+	
+	self.addNode = addNode;
+	function addNode(node) {
+		self.nodes.push(node);
+	}
+	
+	self.addGear = addGear;
+	function addGear() {
+		var newGear = new createNewGear(createGearUI(self.xCenter, self.yCenter));
+		addNode(newGear);
+		newGear.startPulse();
+		newGear.startMoving();
+		var oldAudio = currentAudio;
+		currentAudio = new Audio("audio/RHSummit_"+currentAudioId+".wav");
+		if (oldAudio != null) {
+			oldAudio.mute = true;
+		}
+		currentAudio.loop=true;
+		currentAudio.play();
+		currentAudioId += 1;
+		all_gears.push(newGear);
+	}
+}
+
+function addNode() {
+	var newNode = new createNode(canvas.circle(nodeLocations[nodeId].x, nodeLocations[nodeId].y, 200).attr({fill:'black',stroke:'white'}),nodeLocations[nodeId].x, nodeLocations[nodeId].y);
+	all_nodes.push(newNode);
 }
 
 
@@ -177,10 +216,13 @@ window.onload=function() {
 
 	console.log("Canvas created");
 	canvas = new Raphael(document.getElementById("canvas"), 2000, 2000);
-	canvas.circle(900, 500, 400).attr({fill:'white'});
-	canvas.circle(900, 500, 375).attr({fill:'black'});
-	console.log("Gear created");
-	addGear();
+	//canvas.circle(900, 500, 400).attr({fill:'black', stroke:'white'});
+	//canvas.circle(900, 500, 385).attr({fill:'black'});
+	//console.log("Gear created");
+	//addGear();
+	
+	addNode();
+	all_nodes[0].addGear();
 	
 	
 	setInterval(function() {
@@ -189,7 +231,11 @@ window.onload=function() {
 			//setInterval(all_gears[i].makeMove(), 100);
 		}
 		if (all_gears[0].speed <= 200) {
-			addGear();
+			if (all_nodes[nodeId].nodes.length > 3) {
+				nodeId++;
+				addNode();
+			}
+			all_nodes[nodeId].addGear();
 			for (j=0; j <= all_gears.length -1; j++) {
 				all_gears[j].reset();
 			}
